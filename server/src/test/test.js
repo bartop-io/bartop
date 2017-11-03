e1f9a2a98c3a29b4272290352e4a75054ab87187
Dustin Popp <dpopp07@gmail.com>
2017-11-02 19:11:36 -0500

    add eslint/prettier config to /server

---
+++ server/src/test/test.js
@@ -0,0 +1,14 @@
+var app = require('../server');
+const request = require('supertest');
+const expect = require('chai').expect;
+
+describe('Fake Test!', function(done) {
+  it('should return a silly welcome message', function(done) {
+    request(app)
+      .get('/api/v1/hello')
+      .end(function(err, res) {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+  });
+});
