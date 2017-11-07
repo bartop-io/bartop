dc99133576c9ec4e5a9291013b296cbff9d3ceff
James Clements <jamesvclements@gmail.com>
2017-11-06 23:51:22 -0600

    add reducer test

--- client/src/ducks/app/app.test.js
+++ client/src/ducks/app/app.test.js
@@ -1,11 +1,30 @@
 import { types } from './app';
 import { actions } from './app';
+import reducer from './app';
 
-describe('app actions', () => {
-  it('should create an action to toggle something', () => {
-    const expectedAction = {
-      type: types.TOGGLE_SOMETHING
-    };
-    expect(actions.toggleSomething()).toEqual(expectedAction);
+describe('app duck', () => {
+  describe('actions', () => {
+    it('should create an action to toggle something', () => {
+      const expectedAction = {
+        type: types.TOGGLE_SOMETHING
+      };
+      expect(actions.toggleSomething()).toEqual(expectedAction);
+    });
+  });
+  describe('reducer', () => {
+    it('should return the initial state', () => {
+      expect(reducer(undefined, {})).toEqual({
+        something: false
+      });
+    });
+    it('should handle TOGGLE_SOMETHING', () => {
+      expect(
+        reducer(undefined, {
+          type: types.TOGGLE_SOMETHING
+        })
+      ).toEqual({
+        something: true
+      });
+    });
   });
 });
