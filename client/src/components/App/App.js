580205a0a2448657f8117955c0a36f9b7017ac61
James Clements <jamesvclements@gmail.com>
2017-11-06 23:02:33 -0600

    add redux w/ fake jawn

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import rootReducer from '../../ducks';
 import Landing from '../Landing/Landing';
 import './App.css';
 
+const store = createStore(rootReducer);
+
 const App = () => (
-  <Router>
-    <Route path="/" exact component={Landing} />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Route path="/" exact component={Landing} />
+    </Router>
+  </Provider>
 );
 
 export default App;
