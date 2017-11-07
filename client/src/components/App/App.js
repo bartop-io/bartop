afecb167441ec9234e4c3a01e67afc7d0b8b648f
James Clements <jamesvclements@gmail.com>
2017-11-06 23:09:12 -0600

    enable redux dev tools

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -6,7 +6,13 @@ import rootReducer from '../../ducks';
 import Landing from '../Landing/Landing';
 import './App.css';
 
-const store = createStore(rootReducer);
+// if in development, enable redux dev tools
+const enhancers =
+  process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    : [];
+const store = createStore(rootReducer, enhancers);
 
 const App = () => (
   <Provider store={store}>
