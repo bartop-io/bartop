5b088736501e5cdc01b6168f5449bee97cc51bde
James Clements <james.vincent.clements@ibm.com>
2017-11-08 14:57:15 -0600

    add enzyme

--- client/src/components/Landing/Landing.test.js
+++ client/src/components/Landing/Landing.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import Landing from './Landing';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Landing />, div);
+  shallow(<Landing />);
 });
