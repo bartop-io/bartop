8bce7c60de01d42692c7e6d295516fea4cf2e072
James Clements <james.vincent.clements@ibm.com>
2017-11-08 16:51:30 -0600

    use snapshot testing

--- client/src/components/App/App.test.js
+++ client/src/components/App/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-it('shallow renders without crashing', () => {
-  shallow(<App />);
+it('matches the snapshot', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toMatchSnapshot();
 });
--- client/src/components/Landing/Landing.test.js
+++ client/src/components/Landing/Landing.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Landing from './Landing';
 
-it('shallow renders without crashing', () => {
-  shallow(<Landing />);
+it('matches the snapshot', () => {
+  const wrapper = shallow(<Landing />);
+  expect(wrapper).toMatchSnapshot();
 });
