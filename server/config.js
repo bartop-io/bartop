9bcf92d6a71fa5d1f80d9386a1f6e275c1b7c7f0
Dustin Popp <dpopp07@gmail.com>
2017-11-28 08:15:31 -0600

    enforce environment variables

--- server/config.js
+++ server/config.js
@@ -2,6 +2,14 @@ require('dotenv').config();
 
 const environment = process.env.NODE_ENV || 'development';
 
+if (!envFileIsValid()) {
+  console.error(
+    ' necessary server environment variables not defined\n',
+    'see server/.env.example for the expected variables'
+  );
+  process.exit(1);
+}
+
 let dbName;
 environment === 'test' ? (dbName = 'test') : (dbName = process.env.DB_NAME);
 
@@ -16,3 +24,12 @@ module.exports = {
   },
   env: environment
 };
+
+function envFileIsValid() {
+  return (
+    process.env.API_PORT &&
+    process.env.DB_HOST &&
+    process.env.DB_PORT &&
+    process.env.DB_NAME
+  );
+}
