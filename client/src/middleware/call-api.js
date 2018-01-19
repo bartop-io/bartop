a45387dbd1d8de9684ceeef5ab655feb179d5a77
James Clements <jamesvclements@gmail.com>
2018-01-18 23:30:08 -0600

    Add middleware for actions that call APIs

---
+++ client/src/middleware/call-api.js
@@ -0,0 +1,54 @@
+// Action key that carries API call info interpreted by this Redux middleware.
+export const CALL_API = 'Call API';
+
+// Redux middleware for handling actions that have CALL_API specified
+export default store => next => action => {
+  const callAPI = action[CALL_API];
+  // Pass along actions that aren't for this middleware
+  if (typeof callAPI === 'undefined') {
+    return next(action);
+  }
+
+  const { types, call } = callAPI;
+
+  // Validate that the action has three types (request, success, and failure)
+  if (!Array.isArray(types) || types.length !== 3) {
+    throw new Error('Expected an array of three action types.');
+  }
+  if (!types.every(type => typeof type === 'string')) {
+    throw new Error('Expected action types to be strings.');
+  }
+  // Validate that the action has a function to call (an API fetch)
+  if (!call || typeof call !== 'function') {
+    throw new Error('Expected a call function to execute.');
+  }
+
+  // Shorthand for the actions we'll dispatch along the way
+  const actionWith = data => {
+    const finalAction = Object.assign({}, action, data);
+    delete finalAction[CALL_API];
+    return finalAction;
+  };
+
+  const [requestType, successType, failureType] = types;
+
+  // The request is dispatched immediately
+  next(actionWith({ type: requestType }));
+
+  return call().then(
+    response =>
+      next(
+        actionWith({
+          type: successType,
+          response
+        })
+      ),
+    error =>
+      next(
+        actionWith({
+          type: failureType,
+          error
+        })
+      )
+  );
+};
---
+++ client/src/middleware/call-api.test.js
@@ -0,0 +1,102 @@
+import callAPIMiddleware, { CALL_API } from './call-api';
+
+describe('callAPI middleware', () => {
+  describe('next handler', () => {
+    it('given store, middleware returns function to handle next', () => {
+      const nextHandler = callAPIMiddleware({});
+      expect(typeof nextHandler).toEqual('function');
+      expect(nextHandler.length).toEqual(1);
+    });
+
+    it('returns a function to handle actions', () => {
+      const actionHandler = callAPIMiddleware({})(x => {});
+      expect(typeof actionHandler).toEqual('function');
+      expect(actionHandler.length).toEqual(1);
+    });
+  });
+
+  describe('action handler', () => {
+    let actionHandler = callAPIMiddleware({})(action => {});
+
+    it(`passes along actions if action[CALL_API] is not defined`, done => {
+      const notApiAction = {};
+      actionHandler = callAPIMiddleware({})(action => {
+        expect(action).toBe(notApiAction);
+        done();
+      });
+      actionHandler(notApiAction);
+    });
+
+    it('returns the result of next if action[CALL_API] is not defined', () => {
+      const expected = 'resultOfNext';
+      actionHandler = callAPIMiddleware({})(action => expected);
+      expect(actionHandler({})).toEqual(expected);
+    });
+
+    it('errors if not passed three types', () => {
+      const action = { [CALL_API]: { types: [] } };
+      expect(() => actionHandler(action)).toThrow();
+    });
+
+    it('errors if not passed three string types', () => {
+      const action = { [CALL_API]: { types: ['REQUEST', 'SUCCESS', 0] } };
+      expect(() => actionHandler(action)).toThrow();
+    });
+
+    it('errors if not passed a function to call', () => {
+      const action = {
+        [CALL_API]: {
+          types: ['R', 'S', 'F'],
+          call: 'this should be a function'
+        }
+      };
+      expect(() => actionHandler(action)).toThrow();
+    });
+
+    describe('with a properly structured call api action', () => {
+      const [REQUEST, SUCCESS, FAILURE] = ['REQUEST', 'SUCCESS', 'FAILURE'];
+      const createActionWithCall = call => ({
+        [CALL_API]: {
+          types: [REQUEST, SUCCESS, FAILURE],
+          call
+        }
+      });
+
+      describe('handles a successful API call', () => {
+        const next = jest.fn(action => {});
+        actionHandler = callAPIMiddleware({})(next);
+
+        const response = { data: 'data from API response' };
+        const action = createActionWithCall(() => Promise.resolve(response));
+
+        actionHandler(action);
+
+        it('immediately calls next with the request type', () => {
+          expect(next.mock.calls[0]).toEqual([{ type: REQUEST }]);
+        });
+
+        it('eventually calls next with the success & success response', () => {
+          expect(next.mock.calls[1]).toEqual([{ type: SUCCESS, response }]);
+        });
+      });
+
+      describe('handles a failed API call', () => {
+        const next = jest.fn(action => {});
+        actionHandler = callAPIMiddleware({})(next);
+
+        const error = { message: 'the API call failed' };
+        const action = createActionWithCall(() => Promise.reject(error));
+
+        actionHandler(action);
+
+        it('immediately calls next with the request type', () => {
+          expect(next.mock.calls[0]).toEqual([{ type: REQUEST }]);
+        });
+
+        it('eventually calls next with the success & success response', () => {
+          expect(next.mock.calls[1]).toEqual([{ type: FAILURE, error }]);
+        });
+      });
+    });
+  });
+});
