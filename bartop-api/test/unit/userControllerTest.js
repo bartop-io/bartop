3f602d6b61586195ba4d747a2329627e6779b0ef
Dustin Popp <dpopp07@gmail.com>
2018-03-01 22:40:16 -0600

    slightly optimize user controller test

--- bartop-api/test/unit/userControllerTest.js
+++ bartop-api/test/unit/userControllerTest.js
@@ -1,24 +1,21 @@
 const expect = require('chai').expect;
 const controllerModule = require('../../src/api/user/user.controller');
 const sinon = require('sinon');
-const testObjects = require('../utils/testObjects');
+const { res, users } = require('../utils/testObjects');
 
 describe('users controller - unit test', function(done) {
   it('.create - set correct status code and user object', async function() {
     const mockDb = {};
-    const testUser = testObjects.users.testUser;
-
     mockDb.create = async (tableName, newUser) => {
       return Promise.resolve(newUser);
     };
 
     const req = {
       params: {
-        id: testUser.id
+        id: users.testUser.id
       }
     };
 
-    const res = testObjects.res;
     const jsonSpy = sinon.spy(res, 'json');
     const statusSpy = sinon.spy(res, 'status');
 
@@ -26,7 +23,9 @@ describe('users controller - unit test', function(done) {
     await controller(req, res, null);
 
     expect(jsonSpy.calledOnce).to.equal(true);
-    expect(jsonSpy.alwaysCalledWithExactly({ id: testUser.id })).to.equal(true);
+    expect(jsonSpy.alwaysCalledWithExactly({ id: users.testUser.id })).to.equal(
+      true
+    );
 
     expect(statusSpy.calledOnce).to.equal(true);
     expect(statusSpy.alwaysCalledWithExactly(201)).to.equal(true);
