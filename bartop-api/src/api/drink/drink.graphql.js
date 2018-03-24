2a83121cb9828aa5a0aa1b667f8c61f09ef9217f
Dustin Popp <dpopp07@gmail.com>
2018-03-23 23:22:10 -0500

    add descriptions for graphql schema

--- bartop-api/src/api/drink/drink.graphql.js
+++ bartop-api/src/api/drink/drink.graphql.js
@@ -1,24 +1,44 @@
 module.exports = `
-# primary type
+# Data model representing a cocktail in BarTop
 type Drink {
-  id: String
+
+  # Database-generated key to identify the Drink
+  id: ID
+
+  # Name of the cocktail
   name: String
+
+  # Brief summary describing the cocktail
   description: String
+
+  # List of instructions describing how to make the cocktail
   instructions: [String]
+
+  # List of ingredients used to make the cocktail
   ingredients: [Ingredient]
 }
 
-# sub-types for drink type
+# Describes an ingredient to be used in a cocktail
 type Ingredient {
+
+  # The specific ingredient to use (whiskey, simple syrup, etc)
   ingredient: String
+
+  # Amount object describing how much of the ingredient to use
   amount: Amount
 }
 
+# Describes the amount of an ingredient to use
 type Amount {
-  quantity: Int
+
+  # Numeric quantity of the amount to use
+  quantity: Float
+
+  # Unit that describes the numeric quantity value (oz, dashes, etc)
   unit: String
 }
 
 extend type Query {
+  # Returns a list of all drinks
   listDrinks: [Drink]
 }`;
