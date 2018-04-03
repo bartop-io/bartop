85db29eaeafd4d2a123063aa32b475be09579328
James Clements <james.v.clements@gmail.com>
2018-04-02 19:01:42 -0500

    Center all stories

--- bartop-ui/.storybook/config.js
+++ bartop-ui/.storybook/config.js
@@ -1,9 +1,12 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import centered from '@storybook/addon-centered';
 
 const req = require.context('../src/components', true, /\.stories\.js$/)
 
 function loadStories() {
   req.keys().forEach((filename) => req(filename))
 }
+
+addDecorator(centered);
 
 configure(loadStories, module);
