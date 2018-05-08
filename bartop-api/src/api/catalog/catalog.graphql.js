153077faa2e997c33c78e5a4ac003702330bad06
Dustin Popp <dpopp07@gmail.com>
2018-05-07 21:23:37 -0500

    interface for adding/removing drinks from catalog - not working

--- bartop-api/src/api/catalog/catalog.graphql.js
+++ bartop-api/src/api/catalog/catalog.graphql.js
@@ -1,4 +1,5 @@
 module.exports = `
+
 # Input type containing data needed to create a catalog for a user
 input ReplaceCatalogInput {
 
@@ -19,8 +20,57 @@ type ReplaceCatalogPayload {
   unchanged: Boolean
 }
 
+
+# Input type containing data needed to add a drink to a user's catalog
+input AddDrinkToCatalogInput {
+
+  # The unique ID of the user that corresponds to the catalog to be updated
+  userId: ID!
+
+  # The unique ID of the drink to be added to the catalog
+  drinkId: ID!
+}
+
+# Return type of addDrinkToCatalog
+type AddDrinkToCatalogPayload {
+
+  # The Drink object added to the catalog
+  drink: Drink
+
+  # A flag that will be 'true' if the given drink is already present in the catalog, and 'null' otherwise.
+  unchanged: Boolean
+}
+
+
+# Input type containing data needed to remove a drink from a user's catalog
+input RemoveDrinkFromCatalogInput {
+
+  # The unique ID of the user that corresponds to the catalog to be updated
+  userId: ID!
+
+  # The unique ID of the drink to be removed from the catalog
+  drinkId: ID!
+}
+
+# Return type of removeDrinkFromCatalog
+type RemoveDrinkFromCatalogPayload {
+
+  # The Drink object removed from the catalog
+  drink: Drink
+
+  # A flag that will be 'true' if the given drink was already absent from the catalog, and 'null' otherwise.
+  unchanged: Boolean
+}
+
+
 extend type Mutation {
 
   # Completely replaces an existing catalog with a new one. Should be used to initialize catalogs (by replacing empty ones).
   replaceCatalog(input: ReplaceCatalogInput!): ReplaceCatalogPayload!
+
+  # Add a single drink to a user's catalog, by ID
+  addDrinkToCatalog(input: AddDrinkToCatalogInput!): AddDrinkToCatalogPayload!
+
+  # Remove a single drink from a user's catalog, by ID
+  removeDrinkFromCatalog(input: RemoveDrinkFromCatalogInput!): RemoveDrinkFromCatalogPayload!
 }`;
--- bartop-api/src/api/catalog/catalog.resolvers.js
+++ bartop-api/src/api/catalog/catalog.resolvers.js
@@ -5,8 +5,30 @@ const replaceCatalog = async (root, { input }) => {
   return await logic.replace(input);
 };
 
+const addDrinkToCatalog = async (root, { input }) => {
+  // need from input:
+  // userid of catalog
+  // drinkid to add
+
+  // return:
+  // the single drink, resolved
+  return input;
+};
+
+const removeDrinkFromCatalog = async (root, { input }) => {
+  // need from input:
+  // userid of catalog
+  // drinkid to remove
+
+  // return:
+  // the single drink, resolved
+  return input;
+};
+
 module.exports = {
   Mutation: {
-    replaceCatalog
+    replaceCatalog,
+    addDrinkToCatalog,
+    removeDrinkFromCatalog
   }
 };
