44c0647f96d88d65790eb5075cf9557f6e2bf5c7
James Clements <james.v.clements@gmail.com>
2018-05-16 22:37:25 -0500

    Add generic Button component

---
+++ bartop-ui/src/components/Button/Button.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import Button from './Button';
+
+storiesOf('Button', module).add('default', () => <Button>CLICK ME</Button>);
--- bartop-ui/src/components/NotFound/NotFound.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { storiesOf } from '@storybook/react';
-
-import NotFound from './NotFound';
-
-storiesOf('NotFound', module).add('default', () => <NotFound />);
