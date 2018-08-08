bb93aa6a931c27175bb06a7c570b21174ed87968
Dustin Popp <dpopp07@gmail.com>
2018-08-07 22:23:05 -0500

    add env vars with kubectl command

--- bartop-api/deploy-dev.sh
+++ bartop-api/deploy-dev.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-export NODE_ENV=development
-
 docker build -t gcr.io/${PROJECT_NAME_DEV}/${IMAGE_NAME}:$TRAVIS_COMMIT .
 
 echo $GCLOUD_SERVICE_KEY_DEV | base64 --decode -i > ${HOME}/gcloud-service-key.json
@@ -20,5 +18,16 @@ yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME_DEV}/${IMAGE_NA
 
 kubectl config view
 kubectl config current-context
+
+kubectl set env deployment/${KUBE_DEPLOYMENT_NAME} \
+  NODE_ENV=development \
+  BARTOP_API_PORT=${BARTOP_API_PORT} \
+  BARTOP_DB_HOST_DEV=${BARTOP_DB_HOST_DEV} \
+  BARTOP_DB_PORT=${BARTOP_DB_PORT} \
+  BARTOP_DB_NAME_DEV=${BARTOP_DB_NAME_DEV} \
+  BARTOP_API_AUDIENCE=${BARTOP_API_AUDIENCE} \
+  BARTOP_API_CLIENT_ID=${BARTOP_API_CLIENT_ID} \
+  BARTOP_API_CLIENT_SECRET=${BARTOP_API_CLIENT_SECRET} \
+  BARTOP_API_GRANT_TYPE=${BARTOP_API_GRANT_TYPE} \
 
 kubectl set image deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_CONTAINER_NAME}=gcr.io/${PROJECT_NAME_DEV}/${IMAGE_NAME}:$TRAVIS_COMMIT
